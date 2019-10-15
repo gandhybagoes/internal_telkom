bd0fd47fe40e892821f3ddb91094a96820370fa9
Gandhy Bagoes Cahyono <gbagoescahyono@gmail.com>
2019-10-15 08:41:21 +0700

    remove dead code

--- application/controllers/Status.php
+++ application/controllers/Status.php
@@ -10,96 +10,72 @@ class Status extends CI_Controller
 {
 	function bacaHtml_a($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
 	}
 	function bacaHtml_b($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
 	}
 	function bacaHtml_c($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
 	}
 	function bacaHtml_d($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
 	}
 	function bacaHtml_e($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
 	}
 	function bacaHtml_f($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
 	}
 	function bacaHtml_g($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
 	}
 	function bacaHtml_h($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -11,12 +11,9 @@ class Welcome extends CI_Controller {
 
 	function bacaHtml($url)
 	{
-		// inisialisasi CURL
 	     $data = curl_init();
-	     // setting CURL
 	     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 	     curl_setopt($data, CURLOPT_URL, $url);
-	     // menjalankan CURL untuk membaca isi file
 	     $hasil = curl_exec($data);
 	     curl_close($data);
 	     return $hasil;
